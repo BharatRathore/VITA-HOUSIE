1d0976a307df1809d88d3fb42bd2ca2484d7cc01
bharatrathore <bsrathore0099@gmail.com>
2020-04-28 18:50:17 +0530

    added navbar with prizes and rules info,movies theme added,ticket design changed,added functionality where user can choose the ticket color before printing

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ let countryName=[]
 let capitals=[]
 let districts=[]
 
+//Country,Capitals Data
 fetch('https://restcountries.eu/rest/v2/all')
 .then(response=>{
     return response.json()
@@ -58,7 +59,7 @@ fetch('https://restcountries.eu/rest/v2/all')
 })
 
 
-
+//Indian Districts Data
 fetch('https://indian-cities-api-nocbegfhqg.now.sh/cities')
 .then( response=>{
     return response.json()
@@ -71,10 +72,15 @@ fetch('https://indian-cities-api-nocbegfhqg.now.sh/cities')
     })
 })
 
+//Movie Data
+let movielist=["JULIE","BAAZIGAR","ANMOL","KAALIA","BAAGHI","BICHOO","HAT TRICK","ISHQZAADE","ZUBAIDA","HERA PHERI","STREE","DILWALE","ANAMIKA","DREAM GIRL","KHUSHBOO","DIL CHAHTA HAI","KABIR SINGH","MASOOM","KUDRAT","HINDI MEDIUM","RAM LAKHAN","GULLY BOY","OMKARA","QUEEN","AWAARA","LIFE PARTNER","GURU","NAMASTE LONDON","ARADHANA","ANKAHI","DHOL","PADOSAN","BLACK","ROCK STAR","RAID","TAARE ZAMEEN PAR","WOH KAUN THI","AASHIQUI","DADDY COOL","TAMASHA","PHILLAURI","JODI NO 1","RAABTA","BUDHA MIL GAYA","RACE","DHAMAAL","BARSAAT","TRISHUL","CHAMATKAAR","GOAL","GOOD NEWS","RANGOON","REFUGEE","JODHA AKBAR","BEES SAAL PEHLE","HEROPANTI","LOAFER","ROZA","TAAL","PARDESI BABU","KARAN ARJUN","NO ENTRY","KATI PATANG","KAL HO NA HO","QAYAMAT","KAI PO CHE","FANAA","AAG","JUAARI","PADMAN","RAJU CHACHA","KHAKHEE","APARTMENT","SILSILA","AAR PAAR","SUI DHAGA","LAGAAN","MULK","ASLI NAQLI","WELCOME "]
+
+
 let apiwords={
     countries:countryName,
     capitals:capitals,
-    indianDistricts:districts
+    indianDistricts:districts,
+    movies:movielist
 }
 console.log(apiwords)
 
@@ -114,7 +120,7 @@ let createTicket=()=>{
     test=[]
     let max=wordlist.length-1
     let rand;
-    for(let i=0;i<9;){
+    for(let i=0;i<12;){
         rand=Math.floor(Math.random() * (max + 1) )
         console.log(rand)
         if(!test.includes(rand)){
@@ -126,35 +132,50 @@ let createTicket=()=>{
     }
     
     let coldiv=document.createElement('div')
-    coldiv.setAttribute('class', 'col');
+    coldiv.setAttribute('class', 'row');
+    coldiv.setAttribute('class', 'm-auto');
+    
 
     //generate html ticket containing 9 words
     let ticketHTML='';
     ticketHTML=`
     
-    
-                <div class="ticket-box mb-5 ">
-                            <label>Ticket-${ticketid++}</label>
-                            <div class="row1 d-flex">
-                                <div class="words">${ticket[0]}</div>
-                                <div class="words">${ticket[1]}</div>
-                                <div class="words">${ticket[2]}</div>
-                            </div>
-                            <div class="row1 d-flex">
-                                <div class="words">${ticket[3]}</div>
-                                <div class="words">${ticket[4]}</div>
-                                <div class="words">${ticket[5]}</div>
-                            </div>
-                            <div class="row1 d-flex">
-                                <div class="words">${ticket[6]}</div>
-                                <div class="words">${ticket[7]}</div>
-                                <div class="words">${ticket[8]}</div>
-                            </div>
-                            
-                          
-                        </div>
-                
-    
+    <div class="ml-auto border p-2 shadow rounded mb-5">
+    <div class="d-flex justify-content-between">
+    <label >Ticket no. ${ticketid++}</label>
+    <label class="text-right">Name:______________</label>
+    </div>
+    <table class="tablex avoid  table-bordered table-responsive">
+    <tbody class="avoid">
+    <tr class=" tbl-clr">
+      <td class="align-middle">${ticket[0]}</td>
+      <td class="align-middle">${ticket[1]}</td>
+      <td class="align-middle">${ticket[2]}</td>
+      <td class="align-middle">${ticket[3]}</td>
+      
+      
+    </tr>
+    <tr class=" tbl-clr">
+     <td class="align-middle">${ticket[4]}</td>
+     <td class="align-middle">${ticket[4]}</td>
+     <td class="align-middle">${ticket[6]}</td>
+     <td class="align-middle">${ticket[7]}</td>
+     
+   </tr>
+   <tr class=" tbl-clr">
+     <td class="align-middle">${ticket[8]}</td>
+     <td class="align-middle">${ticket[9]}</td>
+     <td class="align-middle">${ticket[10]}</td>
+     <td class="align-middle">${ticket[11]}</td>
+     
+   </tr>
+   
+   
+    <tbody>
+  </table>
+ 
+           </div>     
+           
     
     
     `
@@ -190,9 +211,30 @@ generateButton.addEventListener('click',e=>{
 })
 
 
+//For color scheme scetion
+
+let clrBtns=document.querySelectorAll('.clr-btn')
+clrBtns.forEach(btn=>{
+    btn.addEventListener('mouseover',e=>{
+        console.log(e.target.id)
+        
+        let tblRows=document.querySelectorAll('.tbl-clr')
+        tblRows.forEach(row=>{
+            row.classList=[]
+            if (e.target.id) {row.classList.add(e.target.id)}
+            row.classList.add('tbl-clr')
+            console.log(row)
+            
+})
+    })
+})
+
+
+
 
 hostbtn.addEventListener('click',()=>{
     hostDiv.style.display="block"
+    console.log("host")
 })
 
 
@@ -225,16 +267,82 @@ wordGenerator.addEventListener('click',e=>{
     
     
 })
-
+let head=document.getElementById('head')
+console.log(head)
 function printDiv() {
     var printContents = document.getElementById('printable').innerHTML;
+    //var bdy=document.getElementsByClassName('wrapper');
     var originalContents = document.body.innerHTML;
+    
+    document.body.innerHTML=printContents
+   
+    window.print()
+    document.body.innerHTML=originalContents
 
-    document.body.innerHTML = printContents;
+    
+ 
 
-    window.print();
-
-    document.body.innerHTML = originalContents;
+    
+        
+    
+    
+        
+        
+    
 }
+
+
+//Rules Section
+function rules(){
+    let rulesObj={
+        1:"A single ticket can win multiple prizes ( Loot Lo Jitna Loot Sako).",
+        2: "If you are the first person to complete then just send a YES / 👍 in the group and then post a photo of the marked ticket in the group along with the original photo that you have received from our end.",
+    
+    3:"We will consider last word called out only. Each word will be called out after a gap of 6 ~ 7 seconds.",
+    
+    4:"In case there is a tie in any particular game, the amount will be divided equally amongst the claimants.",
+    
+    }
+    let rulesModal=document.getElementById('rulesData')
+    let rulesHTML=`
+    
+        <ul >
+        ${Object.keys(rulesObj).map(function (key) {
+            return "<li>" + rulesObj[key] + "</li>"           
+        }).join("")}
+        </ul>
+    
+    `
+    rulesModal.innerHTML+=rulesHTML
+}
+
+//Prizes Section
+// function prizes(){
+//     let prizesObj={
+//         1:"A single ticket can win multiple prizes ( Loot Lo Jitna Loot Sako).",
+//         2: "If you are the first person to complete then just send a YES / 👍 in the group and then post a photo of the marked ticket in the group along with the original photo that you have received from our end.",
+    
+//     3:"We will consider last word called out only. Each word will be called out after a gap of 6 ~ 7 seconds.",
+    
+//     4:"In case there is a tie in any particular game, the amount will be divided equally amongst the claimants.",
+    
+//     }
+//     let prizesModal=document.getElementById('prizesData')
+//     // let prizesHTML=`
+    
+//     //     <ul>
+//     //     ${Object.keys(prizesObj).map(function (key) {
+//     //         return "<li>" + prizesObj[key] + "</li>"           
+//     //     }).join("")}
+//     //     </ul>
+    
+//     // `
+//     //prizesModal.innerHTML+=prizesHTML
+// }
+
+
+rules()
+//prizes()
+
 
 
